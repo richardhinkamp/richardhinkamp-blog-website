20826d59e65f8063edd881f6ec0244aeb53223e0
Richard Hinkamp <richard@hinkamp.nl>
2015-10-19 20:49:29 +0200

    Working with Bolt 2 (except sitemap extension)

--- web/index.php
+++ web/index.php
@@ -1,4 +1,17 @@
 <?php
 
-define('BOLT_WEB_DIR', __DIR__);
-require_once( '../vendor/bolt/bolt/index.php' );
+require_once "../vendor/autoload.php";
+$configuration = new Bolt\Configuration\Composer(dirname(__DIR__));
+$configuration->setPath("web", "web");
+//$configuration->setPath("config", "config");
+$configuration->setPath("files", "web/files");
+$configuration->setPath("themebase", "web/theme");
+//$configuration->getVerifier()->disableApacheChecks();
+$configuration->verify();
+$app = new Bolt\Application(array('resources'=>$configuration));
+$app->initialize();
+$app->run();
+
+
+//define('BOLT_WEB_DIR', __DIR__);
+//require_once( '../vendor/bolt/bolt/index.php' );
